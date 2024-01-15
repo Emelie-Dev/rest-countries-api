485b2cb75ac427c00dc82be29baba37fc6d1b3e3
Emelie-Dev <godfatheremerie@gmail.com>
2024-01-15 01:18:34 +0100

    Fix: Updated some files

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,13 @@
 import React from "react"
 
 
-
 function App() {
   
 
   return (
    
     <div>
+      
       Rest Countries API
      
     </div>
---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import styles from './Header.module.css'
+
+const Header = () => {
+
+  return (
+
+   <header>
+
+    <h2>Where in the world</h2>
+    
+
+   </header>
+
+  )
+}
+
+export default Header
