36d82d1ec6f74a42143d05705df4ae1c9a5caef8
Emelie-Dev <godfatheremerie@gmail.com>
2024-01-24 09:41:32 +0100

    Fix: Updated the route components

--- src/App.jsx
+++ src/App.jsx
@@ -36,8 +36,8 @@ function App() {
         <Header />
 
         <Routes>
-          <Route path="/" element={<Countries />} exact/>
-          <Route path="/country/:country" element={<CountryData />} exact/>
+          <Route path="/" element={<Countries />} />
+          <Route path="/country/:country" element={<CountryData />} />
         </Routes>
       </ModeContext.Provider>
     </>
